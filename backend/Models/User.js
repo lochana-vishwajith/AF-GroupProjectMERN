fd22644ff9fabba5145c0c58657a476f8a3831cd
Savith <savithdissanayake98@gmail.com>
2021-05-25 18:21:35 +0530

    research attributes added

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,48 +1,71 @@
-const mongoose =require('mongoose');
+const mongoose = require("mongoose");
 
 // note that this schema might vary please add Workshop and Research Schemas to this
 //ill create the Backend API after adding Research and Workshop details
 //Please commit after adding model
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    pattern: /.com$/,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    min: 8,
+    required: true,
+  },
+  mobile: {
+    type: Number,
+    required: true,
+  },
+  linkedIn: {
+    type: String,
+  },
+  category: {
+    enum: ["Attendee", "Workshop Conductor", "Researcher"],
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  awards: {
+    type: String,
+  },
+  profilePic: {
+    type: String,
+  },
+  isPayed: {
+    type: Boolean,
+  },
+  workshop: [], //add Workshop related things
+  research: [
+    {
+      researchTitle: {
+        type: String,
+        required: true,
+      },
+      researchField: {
+        type: String,
+        required: true,
+      },
+      researchYear: {
+        type: Date,
+        required: true,
+      },
+      coAuthors: {
+        type: String,
+        required: true,
+      },
+      isAccepted: {
+        type: Boolean,
+        required: true,
+      },
     },
-    email:{
-        type:String,
-        required:true,
-        pattern:/.com$/,
-        lowercase:true
-    },
-    password:{
-        type:String,
-        min:8,
-        required:true
-    },
-    mobile:{
-        type:Number,
-        required:true
-    },
-    linkedIn:{
-        type:String},
-    category:{
-        enum:['Attendee','Workshop Conductor','Researcher'],
-        required:true
-    },
-    description:{
-        type:String,
-        required:true
-    },
-    awards:{
-        type:String
-    },
-    profilePic:{
-        type:String
-    },
-    isPayed:{
-        type:Boolean,
-    },
-    workshop:[],//add Workshop related things
-    research:[]//add Research related things
-
-})
+  ], //add Research related things
+});
