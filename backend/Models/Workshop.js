3925c569f71133e32a78659e5b0bdbb32379468e
ushara <thimalushara@gmail.com>
2021-05-28 10:14:30 +0530

    back end crud completed

--- backend/Models/Workshop.js
+++ backend/Models/Workshop.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 
 const workshopSchema = new mongoose.Schema({
-  wsID: {
-    type: String,
-    required: true,
-  },
-
   wsName: {
     type: String,
     required: true,
